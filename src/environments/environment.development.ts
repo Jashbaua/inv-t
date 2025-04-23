97db34b7bbcbfe0d8ff91ed5df71c32de2a64f05
jashbaua <128972302+Jashbaua@users.noreply.github.com>
2025-04-23 14:51:22 +0530

    env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: false,
     supabaseUrl: 'https://mbitshlenmtzjfovghjg.supabase.co',
     supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1iaXRzaGxlbm10empmb3ZnaGpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5OTQzMjEsImV4cCI6MjA2MDU3MDMyMX0.B8mWCVPKtePZmMewItopPX1mieIaFVBw1P7L46KN4yI'
   };
